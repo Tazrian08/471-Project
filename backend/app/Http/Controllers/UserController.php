a65710769e91543d444103d66dceb043b048a26c
Tazrian Hossain <144312101+Tazrian08@users.noreply.github.com>
2023-10-26 21:35:54 +0600

    Registration

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $user=User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+        return response()->json(['message'=>'User created!','user'=>$user]);
+
+    }
+}
---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  constructor(private http: HttpClient, private router: Router ) 
+  {
+
+  }
+
+
+name: string="";
+email: string="";
+password: string="";
+message:any
+
+
+register()
+  {
+    
+   
+    let bodyData = {
+      "name" : this.name,
+      "email" : this.email,
+      "password" : this.password
+    };
+
+
+    this.http.post("http://localhost:8000/api/register",bodyData).subscribe((resultData: any)=> 
+    {
+
+        alert(resultData["user"].name + " has been registered")
+
+    });
+    console.log("This runs")
+    
+  }
+
+}
