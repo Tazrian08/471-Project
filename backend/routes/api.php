31e9ae408ccb16929a36778965abd531dc302f3a
Tazrian Hossain <144312101+Tazrian08@users.noreply.github.com>
2023-11-14 23:56:31 +0600

    Payment done

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PaypalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,6 +24,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post("/register",[UserController::class,"register"]);
 Route::post("/login",[UserController::class,"login"]);
 
+
+//PAYMENT ROUTES
+Route::post('paypal/payment', [PaypalController::class, 'payment']);
+
+
+//SANCTUM PROTECTED ROUTES
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'user']);
     Route::post('/logout', [UserController::class, 'logout']);
