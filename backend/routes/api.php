b74e13f76e8ced6f5107b0608475bf05ca61b4f3
Wasima Annan <143730734+wasimaannan@users.noreply.github.com>
2023-11-15 21:19:14 +0600

    T_T (2)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -26,5 +28,6 @@ Route::post("/login",[UserController::class,"login"]);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'user']);
     Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/profile', [ProfileController::class, 'profile']);
 
 });
