f5535a98c513959f4b1959e8de30e76b431c1f7f
Wasima Annan <143730734+wasimaannan@users.noreply.github.com>
2023-11-17 06:07:07 +0600

    wut?

--- frontend/src/app/travel-package/travel-package.component.ts
+++ frontend/src/app/travel-package/travel-package.component.ts
@@ -16,7 +16,8 @@ export class TravelPackageComponent{
   ngOnInit(): void {
     this.http.get('http://localhost:8000/api/travel-packages', { withCredentials: true }).subscribe(
       (packages:any) => {
-        this.travelPackages = packages;
+        // Sort the packages by name in alphabetical order
+        this.travelPackages = packages.sort((a: any, b: any) => (a.name < b.name ? -1 : 1)); 
       },
       (error) => {
         console.error('Error fetching travel packages:', error);
