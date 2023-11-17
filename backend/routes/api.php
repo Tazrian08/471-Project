8fd0d5d892f1dd21127af347f83ff76dfc540c7f
Wasima Annan <143730734+wasimaannan@users.noreply.github.com>
2023-11-18 03:09:07 +0600

    WIWI

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -26,6 +26,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //USER ROUTES
 Route::post("/register",[UserController::class,"register"]);
 Route::post("/login",[UserController::class,"login"]);
+
+//TRAVEL PACKAGE ROUTES 
 Route::get("/travel-packages",[TravelPackageController::class,"index"]);
 Route::get("/travel-packages/{id}", [TravelPackageController::class, "show"]);
 
