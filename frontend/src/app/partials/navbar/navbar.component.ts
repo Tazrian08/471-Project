9aa4b5e268ca13ec7b9d9146919b5bb638a11ec1
Tazrian Hossain <144312101+Tazrian08@users.noreply.github.com>
2023-11-20 00:59:44 +0600

    navbar conditions

--- frontend/src/app/partials/navbar/navbar.component.ts
+++ frontend/src/app/partials/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Emitters } from 'src/app/emiters/emitters';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  constructor(private http: HttpClient, private router: Router) {
+
+    Emitters.authEmitter.subscribe(
+      (data: any) => {
+        this.auth= data;
+        console.log("This is working1");
+      }
+    );
+    Emitters.adminEmitter.subscribe(
+      (data: any) => {
+        this.admin= data;
+        console.log("This is working2");
+      }
+    );
+  }
+  auth: any
+  admin: any
+  @Input() Login: boolean = false;
+  @Input() Register: boolean = false ;
+
+
+
+
+  logout(): void {
+    this.http.post('http://localhost:8000/api/logout', {}, {withCredentials: true})
+      .subscribe((succ: any) => {
+        this.auth = false
+        console.log(succ)
+        this.router.navigate(['/login'])
+      });
+  }
 
 }
