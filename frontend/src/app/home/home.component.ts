88bb0e501a4e13c883fafb4762dd64fe177dcce5
Tazrian Hossain <144312101+Tazrian08@users.noreply.github.com>
2023-12-06 18:10:31 +0600

    Home Nav

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { Emitters } from '../emiters/emitters';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  auth:boolean=false
+  admin:boolean=false
+
+
+  constructor(private http: HttpClient, private router: Router) 
+  {
+
+    this.http.get('http://localhost:8000/api/user', {withCredentials: true}).subscribe(
+      (res: any) => {
+        console.log(res)
+        if (res.admin_access==1){
+          Emitters.adminEmitter.emit(true);
+        }
+        Emitters.authEmitter.emit(true);
+      }
+    );
+    Emitters.authEmitter.subscribe(
+      (data: any) => {
+        this.auth= data;
+        console.log("This is working1");
+      }
+    );
+    Emitters.adminEmitter.subscribe(
+      (data: any) => {
+        this.admin= data;
+        console.log("This is working2");
+      }
+    );
+
+
+  }
 
 }
